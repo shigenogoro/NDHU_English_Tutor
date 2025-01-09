c41813a2ba93481edaaa4305da768d5cf3efaaa3
shigenogoro <kyle990987@gmail.com>
2025-01-09 01:32:51 -0500

    Adjust the height of the background image of the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div className="h-screen bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: "url('https://vfis.tdtu.edu.vn/sites/default/files/vfis/network%20university/12.jpg')", backgroundColor: "rgba(0, 0, 0, 0.5)", backgroundBlendMode: "overlay" }}>
+        <div className="min-h-[calc(100vh-4rem)] bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: "url('https://vfis.tdtu.edu.vn/sites/default/files/vfis/network%20university/12.jpg')", backgroundColor: "rgba(0, 0, 0, 0.5)", backgroundBlendMode: "overlay" }}>
             <div className="text-center text-white p-6 bg-opacity-75 bg-gray-800 rounded-lg">
                 <h1 className="text-6xl font-bold mb-6">Welcome to NDHU English Tutoring System</h1>
                 <p className="text-2xl mb-8">
